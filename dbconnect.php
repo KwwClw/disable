1187aa1a5c0679b9d3b9bfa4001017c2d4cfc2ea
Lindist <145182332+Lindist@users.noreply.github.com>
2024-05-17 09:31:20 +0700

    Update dbconnect.php
    OUt put connect

--- dbconnect.php
+++ dbconnect.php
@@ -2,8 +2,8 @@
 
 $connect = mysqli_connect("localhost", "root", "", "disable") or die("Could not connect to database");
 
-// if ($connect) {
-//     echo "connecting";
-// }
+if ($connect) {
+    echo "connecting";
+}
 
 ?>
