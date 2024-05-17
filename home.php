2160e638ed21059236dde2c8977798dc89c17900
KwwClw <neskung50@gmail.com>
2024-05-17 23:40:05 +0700

    update home page
    just page and css

--- home.php
+++ home.php
@@ -1,74 +1,90 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!-- Website - www.codingnepalweb.com -->
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="UTF-8" />
     <title>Home Page</title>
-    <!-- <script src="https://cdn.tailwindcss.com"></script> -->
-    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"> -->
-    <style>
-        @import url('https://fonts.googleapis.com/css2?family=Kanit:wght@300&display=swap');
-        body {
-            font-family: "Kanit", sans-serif;
-            font-weight: 300;
-            font-style: normal;
-        }
-    </style>
-    <link rel="stylesheet" href="style.css">
-    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-</head>
-<body>
+    <link rel="stylesheet" href="style.css" />
+    <!-- Boxicons CDN Link -->
+    <link href="https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css" rel="stylesheet" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  </head>
+  <body>
     <div class="sidebar">
-        <div class="logo_content">
-            <div class="logo">
-                <i class="fa-solid fa-layer-group"></i>
-                <div class="logo_name">Admin</div>
+      <div class="logo-details">
+        <i class="bx bxs-layer icon"></i>
+        <div class="logo_name">Admin</div>
+        <i class="bx bx-menu" id="btn"></i>
+      </div>
+      <ul class="nav-list">
+        <li>
+          <a href="#">
+            <i class="bx bx-user"></i>
+            <span class="links_name">User</span>
+          </a>
+          <span class="tooltip">User</span>
+        </li>
+        <li>
+          <a href="#">
+          <i class='bx bx-table'></i>
+            <span class="links_name">Member Table</span>
+          </a>
+          <span class="tooltip">Member Table</span>
+        </li>
+        <li>
+          <a href="#">
+          <i class='bx bx-table'></i>
+            <span class="links_name">Detail Table</span>
+          </a>
+          <span class="tooltip">Detail Table</span>
+        </li>
+        <li>
+          <a href="#">
+            <i class="bx bx-cog"></i>
+            <span class="links_name">Setting</span>
+          </a>
+          <span class="tooltip">Setting</span>
+        </li>
+        <li class="profile">
+          <div class="profile-details">
+            <img src="https://i.pinimg.com/564x/8b/16/7a/8b167af653c2399dd93b952a48740620.jpg" alt="profileImg" />
+            <div class="name_job">
+              <div class="name">Admin_1</div>
+              <div class="job">Web designer</div>
             </div>
-            <i class="fa-solid fa-bars" id="btn"></i>
-        </div>
-        <ul class="nav_list">
-            <li>
-                <a href="#">
-                    <i class="fa-solid fa-table"></i>
-                    <span class="links_name">detail table</span>
-                </a>
-                <span class="tooltip">detail table</span>
-            </li>
-            <li>
-                <a href="#">
-                    <i class="fa-solid fa-table"></i>
-                    <span class="links_name">member table</span>
-                </a>
-                <span class="tooltip">member table</span>
-            </li>
-        </ul>
-        <div class="profile_content">
-            <div class="profile">
-                <div class="profile_details">
-                    <img src="https://i.pinimg.com/564x/8b/16/7a/8b167af653c2399dd93b952a48740620.jpg" alt="">
-                    <div class="name_job">
-                        <div class="name">Admin1</div>
-                        <div class="job">Web Designer</div>
-                    </div>
-                </div>
-                <i class='bx bx-log-out' id="log_out" ></i>
-            </div>
-        </div>
+          </div>
+          <i class="bx bx-log-out" id="log_out"></i>
+        </li>
+      </ul>
     </div>
-    <div class="home_content">
-        <div class="text">Home Content</div>
-    </div>
+    <section class="home-section">
+      <div class="text">Dashboard</div>
+    </section>
 
     <script>
-        let btn = document.querySelector('#btn');
-        let sidebar = document.querySelector('.sidebar');
+        let sidebar = document.querySelector(".sidebar");
+        let closeBtn = document.querySelector("#btn");
+        let searchBtn = document.querySelector(".bx-search");
 
-        btn.onclick = function() {
-            sidebar.classList.toggle("active")
+        closeBtn.addEventListener("click", ()=>{
+        sidebar.classList.toggle("open");
+        menuBtnChange();//calling the function(optional)
+        });
+
+        searchBtn.addEventListener("click", ()=>{ // Sidebar open when you click on the search iocn
+        sidebar.classList.toggle("open");
+        menuBtnChange(); //calling the function(optional)
+        });
+
+        // following are the code to change sidebar button(optional)
+        function menuBtnChange() {
+        if(sidebar.classList.contains("open")){
+        closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");//replacing the iocns class
+        }else {
+        closeBtn.classList.replace("bx-menu-alt-right","bx-menu");//replacing the iocns class
         }
-
+        }
     </script>
 
-</body>
+  </body>
 </html>
